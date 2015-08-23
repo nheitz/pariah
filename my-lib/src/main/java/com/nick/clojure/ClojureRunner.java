96f1dbf2dc2c35890dae30314d30fa8c0d60bed0
Nick Heitz <heitzsub@gmail.com>
2015-08-23 21:10:45 +0100

    done

--- my-lib/src/main/java/com/nick/clojure/ClojureRunner.java
+++ my-lib/src/main/java/com/nick/clojure/ClojureRunner.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2012 Pivotal Software, Inc.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- * Pivotal Software, Inc. - initial API and implementation
- *******************************************************************************/
 package com.nick.clojure;
 
 import javax.script.ScriptEngineManager;
@@ -23,7 +13,7 @@ public class ClojureRunner {
 
 	public void callClojure() {
 		ScriptEngineManager scriptManager = new ScriptEngineManager();
-		
+
 		IFn plus = Clojure.var("clojure.core", "+");
 		IFn println = Clojure.var("clojure.core", "println");
 
